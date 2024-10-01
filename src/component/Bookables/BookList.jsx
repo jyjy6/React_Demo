b77c6578149db8060ad7195adfb7e378c210f178
jyjy <jungjuneyoung6@gmail.com>
2024-10-01 18:17:57 +0900

    #4 BookList Select Changed

--- src/component/Bookables/BookList.jsx
+++ src/component/Bookables/BookList.jsx
@@ -2,18 +2,23 @@ import { useState } from "react";
 import { bookables } from "../../static.json";
 
 function BookList() {
-  const group = "Rooms";
-
+  const [group, setGroup]= useState("Rooms")
   const bookableGroup = bookables.filter((b) => b.group === group); // return 문 필요 없음
   const [bookableIndex, setBookableIndex] = useState(0)
-  function nextBookableIndex(){
-    setBookableIndex(i=>i+1);
-  }
+  // function nextBookableIndex(){
+  //   setBookableIndex(i=>i+1);
+  // }
 
+  const groups = Array.from(new Set(bookables.map(item => item.group)))
 
   return (
     <>
       <ul className="items-list-nav">
+      <select onChange={(e) => setGroup(e.target.value)} value={group}>
+        {groups.map(g => (
+          <option value={g} key={g}>{g}</option>
+        ))}
+      </select>
         {bookableGroup.map((b, i) => (
             <li key={b.id} className={i === bookableIndex ? "selected" : ""}>
               <button className="btn" onClick={() => setBookableIndex(i)}>
